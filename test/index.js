8168b2d9e49eb3dab76f8787297b84e070394a37
Nova <novka666@mail.ru>
2019-02-27 01:54:08 +0300

    test: add packages

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const server = require('../routes');
+
+describe('Basic Mocha String Test', function () {
+ it('should return number of charachters in a string', function () {
+        
+    });
+ it('should return first charachter of the string', function () {
+        
+    });
+});
